5507044d62d8c3614a7b828dadfef84274ec7159
art2cat <yiming.whz@gmail.com>
2018-01-14 18:47:03 +0800

    add raspi script

---
+++ raspi/mount_usb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -d /mnt/usbstorage ]
+then 
+    sudo mkdir /mnt/usbstorage
+fi
+
+read -r -t 60 -p "mount or umount(M/N):" option
+option=${option,,}
+if [[ $option == "m" ]] 
+then 
+    sudo mount -o uid=pi,gid=pi /dev/sda1 /mnt/usbstorage
+elif [[ $option == "n" ]]
+then 
+    sudo umount /dev/sda1
+else
+    echo "enter "M/N" to mount or umount device!"
+fi
+
