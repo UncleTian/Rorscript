f1c960323bfd334cb39078afe98cfa184acca89c
art2cat <yiming.whz@gmail.com>
2018-02-01 10:13:21 +0800

    add rmjdk.sh

---
+++ remove/rmjdk.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# run as root 
+# Changed code to remove the 'head -1' as per the suggestion in comment.
+JAVA_VERSION=`java -version 2>&1 |awk 'NR==1{ gsub(/"/,""); print $3 }'`
+# export JAVA_VERSION
+echo $JAVA_VERSION
+echo 'jdk'$JAVA_VERSION'.jdk'
+rm -rfv /Library/Java/JavaVirtualMachines/jdk$JAVA_VERSION.jdk
+rm -rfv /Library/PreferencePanes/JavaControlPanel.prefPane
+rm -rfv /Library/Internet/Plug-Ins/JavaAppletPlugin.plugin
+rm -rfv /Library/LaunchAgents/com.oracle.java.Java-Updater.plist
+rm -rfv /Library/PrivilegedHelperTools/com.oracle.java.JavaUpdateHelper
+rm -rfv /Library/LaunchDaemons/com.oracle.java.Helper-Tool.plist
+rm -rfv /Library/Preferences/com.oracle.java.Helper-Tool.plist
