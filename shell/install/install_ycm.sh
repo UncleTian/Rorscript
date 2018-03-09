3f5872ddc76ac3703ec5270f3ae451b2836fcc5f
art2cat <yiming.whz@gmail.com>
2018-03-09 17:11:02 +0800

    update

--- shell/install/install_ycm.sh
+++ shell/install/install_ycm.sh
@@ -6,6 +6,11 @@ sudo apt install build-essential cmake
 
 sudo apt install python-dev python3-dev
 
+cd ~/.vim/bundle/
+
+git clone https://github.com/Valloric/YouCompleteMe.git
+
 cd ~/.vim/bundle/YouCompleteMe
+
 ./install.py --clang-completer
 ./install.py --go-completer
