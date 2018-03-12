7b9377cb7e6b81b31a745a1d68c309034725e5b6
art2cat <yiming.whz@gmail.com>
2018-03-12 13:11:44 +0800

    update

--- shell/install/build_vim.sh
+++ shell/install/build_vim.sh
@@ -1,11 +1,16 @@
 #!/bin/bash 
 
-git clone git@github.com:vim/vim.git ~/vim/
+git clone https://github.com/vim/vim.git ~/vim/
 
 cd ~/vim/
-
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "configure vim"
+echo
 ./configure --with-features=huge --enable-pythoninterp --enable-rubyinterp --enable-luainterp --enable-perlinterp --with-python-config-dir=/usr/lib/python2.7/config/ --enable-gui=gtk2 --enable-cscope --prefix=/usr 
 
-make
-
-make install
+echo
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+sudo make
+echo
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+sudo make install
--- shell/install/install_clang.sh
+++ shell/install/install_clang.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "install clang 6.0 on debian stretch"
-sudo apt-add-repository deb http://apt.llvm.org/stretch/ llvm-toolchain-stretch-6.0 main
+sudo sh -c "echo 'deb http://apt.llvm.org/stretch/ llvm-toolchain-stretch-6.0 main' >> /etc/apt/sources.list"
 
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add - # Fingerprint: 6084 F3CF 814B 57C1 CF12 EFD5 15CF 4D18 AF4F 7421
 
@@ -9,10 +9,10 @@ sudo apt update
 
 sudo apt install clang-6.0 clang-tools-6.0 clang-6.0-doc libclang-common-6.0-dev libclang-6.0-dev libclang1-6.0 libclang1-6.0-dbg libllvm6.0 libllvm6.0-dbg lldb-6.0 llvm-6.0 llvm-6.0-dev llvm-6.0-doc llvm-6.0-runtime clang-format-6.0 python-clang-6.0 libfuzzer-6.0-dev -y
 
-update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-6.0 1000
-update-alternatives --install /usr/bin/clang clang /usr/bin/clang-6.0 1000
-update-alternatives --config clang
-update-alternatives --config clang++
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-6.0 1000
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-6.0 1000
+sudo update-alternatives --config clang
+sudo update-alternatives --config clang++
 
 echo "configure clang-format"
 sudo cp /usr/bin/clang-format-6.0 /usr/bin/clang-format
