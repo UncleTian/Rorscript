15d109a81592360a29df81bea6cba1688bfcdda2
art2cat <yiming.whz@gmail.com>
2018-05-03 16:33:08 +0800

    update generate_java_bean.

--- python/generate_java_bean.py
+++ python/generate_java_bean.py
@@ -10,9 +10,10 @@ from xml.etree.ElementTree import Element, SubElement, Comment
 
 class JavaBean(object):
 
-    def __init__(self, bean_name, ref_class, properties):
+    def __init__(self, bean_name, ref_class, parent_bean, properties):
         self.bean_name = bean_name
         self.ref_class = ref_class
+        self.parent_bean = parent_bean
         self.properties = properties
 
 
@@ -38,26 +39,31 @@ def get_java_bean_set(source_dir):
     java_bean_set = set([])
     re_prop = re.compile(
         r"[\(\s]*([\w]+)[\)\s]+BeanUtils[\s\.]+"
-        r"getBean[\(\s]+\"([\w]+)\"[\s]*\)")
-    re_package = re.compile(r"^package\s(.+);$")
+        r"getBean[\(\s]+\"(.+)\"[\s]*\)")
+    re_package = re.compile(r"package\s(.+);")
+    re_parent = re.compile(r"\w\sextends\s?(\w+)")
     for file_name in java_files:
         properties = {}
         file_path = os.path.join(source_dir, file_name)
         bean_name = first_lower(file_name)[:-5]
         ref_class = ""
-        java_bean = JavaBean(bean_name, ref_class, properties)
+        parent = ""
+        java_bean = JavaBean(bean_name, ref_class, parent, properties)
 
         with open(file_path, "r") as f:
-            all_line = f.readlines()
-            for line in all_line:
-                result = re_package.search(line)
-                if result is not None and result.groups():
-                    ref_class = result.group(1)
-                result = re_prop.search(line)
-                if result is not None and result.groups():
-                    properties[result.group(1)] = result.group(2)
+            content = f.read()
+            result = re_package.search(content)
+            if result is not None and result.groups():
+                ref_class = result.group(1)
+            result = re_prop.search(content, re.MULTILINE)
+            if result is not None and result.groups():
+                properties[result.group(1)] = result.group(2)
+            result = re_parent.search(content)
+            if result is not None and result.groups():
+                parent = result.group(1)
             java_bean.ref_class = ref_class + "." + file_name[:-5]
             java_bean.properties = properties
+            java_bean.parent_bean = parent
         java_bean_set.add(java_bean)
     return java_bean_set
 
@@ -73,6 +79,8 @@ def generate_bean_xml(java_bean_set):
         bean = SubElement(beans, "bean")
         bean.set("aid", i.bean_name)
         bean.set("class", i.ref_class)
+        if i.parent_bean != "":
+            bean.set("parent", first_lower(i.parent_bean))
         if len(i.properties) != 0:
             for key, value in i.properties.items():
                 property = SubElement(bean, "property")
@@ -83,30 +91,11 @@ def generate_bean_xml(java_bean_set):
     return prettify(beans)
 
 
-def generate_list_bean_xml(java_bean_set):
-    # Configure one attribute with set()
-    beans = Element("beans")
-
-    comment = Comment("Generated for Python")
-    beans.append(comment)
-
-    bean = SubElement(beans, "map")
-    for i in java_bean_set:
-
-        entry = SubElement(bean, "entry")
-        entry.set("key", i.ref_class)
-        entry.set("value-ref", i.bean_name)
-
-    print(prettify(beans))
-    return prettify(beans)
-
-
 def main():
     current_dir = os.path.abspath(".")
     java_bean_set = get_java_bean_set(current_dir)
     with open(os.path.join(current_dir, "spring.xml"), "w") as f:
         f.write(generate_bean_xml(java_bean_set))
-        f.write(generate_list_bean_xml(java_bean_set))
 
 
 
