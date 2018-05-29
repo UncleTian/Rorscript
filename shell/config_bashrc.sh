f577963a1d744f2c98cff0e924f0a9d031718d63
art2cat <yiming.whz@gmail.com>
2018-05-29 16:57:17 +0800

    add config_bashrc.

---
+++ shell/config_bashrc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $UID -ne 0 ]; 
+then
+	echo "Not root user. Please run as root."
+	exit
+else 
+	echo "Root user."
+fi
+
+echo "add backup dir for rm."
+mkdir ~/.backup
+echo "alias rm='cp $@ ~/.backup && rm $@'" >> ~/.bashrc
+
+echo "prepend() { [ -d \"$2\"] && eval $1=\\\"%2':'\$$1\\\" && export $1; }" >> ~/.bashrc
