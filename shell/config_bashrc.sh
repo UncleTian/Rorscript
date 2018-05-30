c549b957df33fb09e6aa4f9ef6730e836b8618ff
art2cat <yiming.whz@gmail.com>
2018-05-30 16:26:16 +0800

    update config_bashrc/

--- shell/config_bashrc.sh
+++ shell/config_bashrc.sh
@@ -9,7 +9,13 @@ else
 fi
 
 echo "add backup dir for rm."
-mkdir ~/.backup
-echo "alias rm='cp \$@ ~/.backup && rm \$@'" >> ~/.bashrc
+mkdir -p ~/.trash
+echo "\n" >> ~/.bashrc
+echo "alias rm=movetotrash" >> ~/.bashrc
+echo "alias urm=unrmfile" >> ~/.bashrc
+echo "alias cltrash=cleartrash" >> ~/.bashrc
+echo "movetotrash() {\n\tmv \$@ ~/.trash/ \n}" >> ~/.bashrc
+echo "unrmfile() {\n\tmv -i ~/.trash/\$@ ./ \n}" >> ~/.bashrc
+echo "cleartrash() {\n\tread -p \"Clear trash?(Y/n)\" confirm\n\tconfirm=\${confirm,,}\n\t[ \$confirm == 'y' ] && /usr/bin/rm -rf ~/.trash/\*\n}" >> ~/.bashrc
 
-echo "prepend() { [ -d \"\$2\"] && eval \$1=\\\"\$2':'\$\$1\\\" && export \$1; }" >> ~/.bashrc
+echo "prepend() {\n\t [ -d \"\$2\"] && eval \$1=\\\"\$2':'\$\$1\\\" && export \$1; \n}" >> ~/.bashrc
