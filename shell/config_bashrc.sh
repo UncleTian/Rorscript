7efe314eaf12292b14e825e653cc3ff25fee31db
art2cat <yiming.whz@gmail.com>
2018-05-31 16:11:55 +0800

    add disk_monitor.

--- shell/config_bashrc.sh
+++ shell/config_bashrc.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-if [ $UID -ne 0 ]; 
-then
-	echo "Not root user. Please run as root."
-	exit
-else 
-	echo "Root user."
-fi
+# if [ $UID -ne 0 ]; 
+# then
+# 	echo "Not root user. Please run as root."
+# 	exit
+# else 
+# 	echo "Root user."
+# fi
 
 echo "add backup dir for rm."
-mkdir -p ~/.trash
-echo "\n" >> ~/.bashrc
+sudo mkdir -p ~/.trash
+echo "\n" >> ~/.testfile
+
 echo "alias rm=movetotrash" >> ~/.bashrc
 echo "alias urm=unrmfile" >> ~/.bashrc
-echo "alias cltrash=cleartrash" >> ~/.bashrc
-echo "movetotrash() {\n\tmv \$@ ~/.trash/ \n}" >> ~/.bashrc
-echo "unrmfile() {\n\tmv -i ~/.trash/\$@ ./ \n}" >> ~/.bashrc
-echo "cleartrash() {\n\tread -p \"Clear trash?(Y/n)\" confirm\n\tconfirm=\${confirm,,}\n\t[ \$confirm == 'y' ] && /usr/bin/rm -rf ~/.trash/\*\n}" >> ~/.bashrc
+echo -e "alias cltrash=cleartrash\n" >> ~/.bashrc
+echo -e "movetotrash() {\n\tmv \$@ ~/.trash/ \n}\n" >> ~/.bashrc
+echo -e "unrmfile() {\n\tmv -i ~/.trash/\$@ ./ \n}\n" >> ~/.bashrc
+echo -e "cleartrash() {\n\tread -p \"Clear trash?(Y/n)\" confirm\n\tconfirm=\${confirm,,}\n\t[ \$confirm == 'y' ] && /usr/bin/rm -rf ~/.trash/\*\n}\n" >> ~/.bashrc
 
-echo "prepend() {\n\t [ -d \"\$2\"] && eval \$1=\\\"\$2':'\$\$1\\\" && export \$1; \n}" >> ~/.bashrc
+echo -e "prepend() {\n\t [ -d \"\$2\"] && eval \$1=\\\"\$2':'\$\$1\\\" && export \$1; \n}\n" >> ~/.bashrc
