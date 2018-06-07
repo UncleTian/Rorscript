578bbb4c26fb7e0c389aaca9338155df039381bd
art2cat <yiming.whz@gmail.com>
2018-06-07 04:39:13 -0400

    update install_java

--- shell/install/install_java.sh
+++ shell/install/install_java.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 # install jdk on ubuntu.
-sudo mkdir /usr/lib/jvm
 
-wget -O http://download.oracle.com/otn-pub/java/jdk/9.0.4+11/c2514751926b4512b076cc82f959763f/jdk-9.0.4_linux-x64_bin.tar.gz?AuthParam=1516608341_3343898786f6f1b4fa420f46ce81b023
+function mkjvmdir() {
+	jvm=/user/lib/jvm
+	if [ -e $jvm ]; then
+		sudo mkdir -p $jvm
+	fi
+}
 
-sudo tar -zxvf jdk-9.0.4_linux-x64_bin.tar.gz -C /usr/lib/jvm
+read -p "openjdk or oracle jdk(1/2)" response
+if [[ $response -eq 1 ]]; then
+	sudo apt update && sudo apt install openjdk-8-jdk -y 
+else
+	mkjvmdir
+	wget -O 'http://download.oracle.com/otn-pub/java/jdk/9.0.4+11/c2514751926b4512b076cc82f959763f/jdk-9.0.4_linux-x64_bin.tar.gz?AuthParam=1516608341_3343898786f6f1b4fa420f46ce81b023'
 
-# set oracle jdk environment
-echo "export JAVA_HOME=/usr/lib/jvm/jdk-9.0.4" >> ~/.bashrc
-echo "export JRE_HOME=${JAVA_HOME}/jre" >> ~/.bashrc
-echo "export CLASSPAHT=.:${JAVA_HOME}/lib:${JRE_HOME}/lib" >> ~/.bashrc
-echo "export PATH=${JAVA_HOME}/bin:$PATH" >> ~/.bashrc
+	sudo tar -zxvf jdk-9.0.4_linux-x64_bin.tar.gz -C /usr/lib/jvm
+
+	# set oracle jdk environment
+	echo "export JAVA_HOME=/usr/lib/jvm/jdk-9.0.4" >> ~/.bashrc
+	echo "export JRE_HOME=${JAVA_HOME}/jre" >> ~/.bashrc
+	echo "export CLASSPAHT=.:${JAVA_HOME}/lib:${JRE_HOME}/lib" >> ~/.bashrc
+	echo "export PATH=${JAVA_HOME}/bin:$PATH" >> ~/.bashrc
+fi
 
 source ~/.bashrc
 
-java --version
+java -version
 
 
 
